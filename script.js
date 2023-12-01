1109963cd1131171a93f4e999c979fdcac79b68b
Mauricio Jacobsem <mauriciojacobsen55@gmail.com>
2023-12-01 00:18:49 -0300

    feat: calculator

---
+++ script.js
@@ -0,0 +1,24 @@
+function insert (num) {
+    let numero = document.getElementById('res').innerHTML
+    document.getElementById('res').innerHTML = numero + num
+}
+
+function clean() {
+    document.getElementById('res').innerHTML = ''
+}
+
+function back() {
+    let resultado = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+    let resultado = document.getElementById('res').innerHTML;
+        if(resultado != 0) {
+        document.getElementById('res').innerHTML = eval(resultado)
+    }
+        else{
+            document.getElementById('res').innerHTML = ''
+        }
+    
+}
